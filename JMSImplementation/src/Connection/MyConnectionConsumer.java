374f4f153b9f84276204eecd93cf28e4dbda1fd9
Eduardo Pires <eap3@cin.ufpe.br>
2016-06-21 12:02:56 -0300

    Connection interfaces

---
+++ JMSImplementation/src/Connection/MyConnectionConsumer.java
@@ -0,0 +1,22 @@
+package Connection;
+
+import javax.jms.ConnectionConsumer;
+import javax.jms.JMSException;
+import javax.jms.ServerSessionPool;
+
+public class MyConnectionConsumer implements ConnectionConsumer {
+
+	ServerSessionPool serverSessionPool;
+	
+	@Override
+	public void close() throws JMSException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ServerSessionPool getServerSessionPool() throws JMSException {
+		return serverSessionPool;
+	}
+
+}
