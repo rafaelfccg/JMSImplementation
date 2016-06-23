70dfbf693914058fcbd01a0a8e0028ccf8fcd758
Eduardo Pires <eap3@cin.ufpe.br>
2016-06-23 09:32:20 -0300

    message classes implemented

---
+++ JMSImplementation/src/Messages/MyMessageListener.java
@@ -0,0 +1,14 @@
+package Messages;
+
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+public class MyMessageListener implements MessageListener{
+
+	@Override
+	public void onMessage(Message arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
