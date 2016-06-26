c12d294f5e28ba78dcade2badbb08f8f01aefb76
rafaelfccg <rafaelfccg@gmail.com>
2016-06-26 09:34:22 -0300

    Small fix

--- JMSImplementation/src/topic/MyTopicSession.java
+++ JMSImplementation/src/topic/MyTopicSession.java
@@ -6,9 +6,15 @@ import javax.jms.TopicPublisher;
 import javax.jms.TopicSession;
 import javax.jms.TopicSubscriber;
 
+import connection.MyConnectionSendMessage;
 import session.MySession;
 
 public class MyTopicSession extends MySession implements TopicSession{
+
+	public MyTopicSession(boolean trans, int ack, MyConnectionSendMessage connection) {
+		super(trans, ack, connection);
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
 	public TopicPublisher createPublisher(Topic arg0) throws JMSException {
