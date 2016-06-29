434ec2220483efad8be92d31655eb286ecf27db0
Tomer Simis <tls@cin.ufpe.br>
2016-06-29 19:57:41 -0300

    Including JNDI with RMI provider in the server launcher

--- JMSImplementation/src/ServerLauncher.java
+++ JMSImplementation/src/ServerLauncher.java
@@ -1,10 +1,35 @@
+import java.rmi.registry.LocateRegistry;
+import java.util.Hashtable;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NameClassPair;
+import javax.naming.NamingEnumeration;
+import javax.naming.Reference;
+import javax.naming.StringRefAddr;
+
 import server.Server;
+
+/**
+ * To use the JNDI service:
+ * Hashtable<String, String> env = new Hashtable();
+ * env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.rmi.registry.RegistryContextFactory");
+ * env.put(Context.PROVIDER_URL, "rmi://127.0.0.1:1099");
+ * Context context = new InitialContext(env);
+ *
+ * All binded objects MUST implement Remote and Serializable
+ *
+ */
 
 public class ServerLauncher {
 	
 	public static void main(String[] args) throws Exception{
+		
+		LocateRegistry.createRegistry(1099);
+		
 		Server server = new Server(12345);
 		server.init();
+		
 	}
 	
 }
