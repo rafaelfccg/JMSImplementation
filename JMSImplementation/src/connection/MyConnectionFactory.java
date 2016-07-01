62a2ea5229ed5d4057db34fed54805322734dd6e
rafaelfccg <rafaelfccg@gmail.com>
2016-07-01 09:19:03 -0300

    Connection factory

--- JMSImplementation/src/connection/MyConnectionFactory.java
+++ JMSImplementation/src/connection/MyConnectionFactory.java
@@ -5,17 +5,24 @@ import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
 
 public class MyConnectionFactory implements ConnectionFactory{
+	
+	private String providerIp;
+	private String providerPort;
+	
+	public MyConnectionFactory(String ip, String port) {
+		this.providerIp = ip;
+		this.providerPort = port;
+	}
 
 	@Override
 	public Connection createConnection() throws JMSException {
-		// TODO Auto-generated method stub
-		return null;
+		return (Connection) new MyConnectionFactory(this.providerIp, this.providerPort);
 	}
 
 	@Override
 	public Connection createConnection(String arg0, String arg1) throws JMSException {
-		// TODO Auto-generated method stub
-		return null;
+		// TODO  Auth
+		throw new JMSException("Authentication not Implemented yet");
 	}
 
 }
