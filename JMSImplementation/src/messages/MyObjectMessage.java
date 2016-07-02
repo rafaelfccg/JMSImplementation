3c8077476ec1170ab1fcc0fed7adca7e40537c2e
rafaelfccg <rafaelfccg@gmail.com>
2016-07-02 18:07:09 -0300

    fix on Object Message

--- JMSImplementation/src/messages/MyObjectMessage.java
+++ JMSImplementation/src/messages/MyObjectMessage.java
@@ -9,12 +9,18 @@ public class MyObjectMessage extends MyMessage implements ObjectMessage{
 	Serializable object;
 	
 	@Override
+	public void clearBody() throws JMSException {
+		this.object = null;
+	};
+	
+	@Override
 	public Serializable getObject() throws JMSException {
 		return object;
 	}
 
 	@Override
 	public void setObject(Serializable arg0) throws JMSException {
+		isSettable();
 		this.object = arg0;
 		
 	}
